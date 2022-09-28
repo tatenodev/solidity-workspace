b3f7c8ddf169a802e9dcb55fcb1a1fcbcac4b79a
tatenodev <gd28.c.tateno@gmail.com>
2022-09-28 14:50:34 +0900

    add Emitter

---
+++ blockchain/contracts/Emitter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+abstract contract Emitter {
+  struct Transactor {
+    address sender;
+    address reciever;
+    uint256 amount;
+  }
+
+  Transactor[] list;
+
+  // 最大3つまでindexed修飾子を追加できる
+  event add(address indexed from, address indexed reciever, uint256 amount);
+
+  function addList(address _reciver, uint256 _amount) public {
+    list.push(Transactor(msg.sender, _reciver, _amount));
+    emit add(msg.sender, _reciver, _amount);
+  }
+}
